091a2c76f4b6b48f9bd7b3b8343d0efa27633789
Pranesh Hanumanth <praneshhanumanth@Praneshs-MacBook-Pro.local>
2019-06-10 19:51:18 -0400

    fixed tsLint issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule, Injector } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TestOneComponent } from "./test-one/test-one.component";
-import { TestTwoComponent } from "./test-two/test-two.component";
-import { TestThreeComponent } from "./test-three/test-three.component";
-import { createCustomElement } from "@angular/elements";
-import { AppComponent } from "./app/app.component";
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestOneComponent } from './test-one/test-one.component';
+import { TestTwoComponent } from './test-two/test-two.component';
+import { TestThreeComponent } from './test-three/test-three.component';
+import { createCustomElement } from '@angular/elements';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   imports: [BrowserModule, BrowserAnimationsModule],
@@ -27,12 +27,12 @@ import { AppComponent } from "./app/app.component";
 export class AppModule {
   constructor(injector: Injector) {
     const TestOneElement = createCustomElement(TestOneComponent, { injector });
-    customElements.define("app-test-one", TestOneElement);
+    customElements.define('app-test-one', TestOneElement);
     const TestTwoElement = createCustomElement(TestTwoComponent, { injector });
-    customElements.define("app-test-two", TestTwoElement);
+    customElements.define('app-test-two', TestTwoElement);
     const TestThreeElement = createCustomElement(TestThreeComponent, {
       injector
     });
-    customElements.define("app-test-three", TestThreeElement);
+    customElements.define('app-test-three', TestThreeElement);
   }
 }
