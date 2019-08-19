cb9d9753aff50ecebfad41bbdb8f0e3f2f7702d9
Pranesh Hanumanth <praneshhanumanth@Praneshs-MacBook-Pro.local>
2019-08-19 12:14:27 -0400

    Bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,33 @@
-import { NgModule, Injector } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TestOneComponent } from "./test-one/test-one.component";
-import { TestTwoComponent } from "./test-two/test-two.component";
-import { TestThreeComponent } from "./test-three/test-three.component";
-import { createCustomElement } from "@angular/elements";
+import { AppComponent } from './app.component';
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestOneComponent } from './test-one/test-one.component';
+import { TestTwoComponent } from './test-two/test-two.component';
+import { TestThreeComponent } from './test-three/test-three.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  declarations: [TestOneComponent, TestTwoComponent, TestThreeComponent],
-  bootstrap: [],
+  declarations: [
+    TestOneComponent,
+    TestTwoComponent,
+    TestThreeComponent,
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
   entryComponents: [TestOneComponent, TestTwoComponent, TestThreeComponent]
 })
 export class AppModule {
   constructor(injector: Injector) {
     const TestOneElement = createCustomElement(TestOneComponent, { injector });
-    customElements.define("app-test-one", TestOneElement);
+    customElements.define('app-test-one', TestOneElement);
     const TestTwoElement = createCustomElement(TestTwoComponent, { injector });
-    customElements.define("app-test-two", TestTwoElement);
+    customElements.define('app-test-two', TestTwoElement);
     const TestThreeElement = createCustomElement(TestThreeComponent, {
       injector
     });
-    customElements.define("app-test-three", TestThreeElement);
+    customElements.define('app-test-three', TestThreeElement);
   }
 }
